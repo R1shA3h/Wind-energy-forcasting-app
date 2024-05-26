528230dd78f2423f3acb2029c21d30b1b92dbdd1
Rishabh <r1sha3hjain@gmail.com>
2024-05-27 00:29:57 +0530

    first commit

---
+++ prisma/migrations/20240526170437_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "WindActual" (
+    "id" SERIAL NOT NULL,
+    "dataset" TEXT NOT NULL,
+    "publishTime" TIMESTAMP(3) NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "settlementPeriod" INTEGER NOT NULL,
+    "fuelType" TEXT NOT NULL,
+    "generation" INTEGER NOT NULL,
+
+    CONSTRAINT "WindActual_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WindForecast" (
+    "id" SERIAL NOT NULL,
+    "dataset" TEXT NOT NULL,
+    "publishTime" TIMESTAMP(3) NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "generation" INTEGER NOT NULL,
+
+    CONSTRAINT "WindForecast_pkey" PRIMARY KEY ("id")
+);
